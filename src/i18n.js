eba3e8e04589cc20271433855cc488dc3265a3c5
Mathis S.M. <contact@minarox.fr>
2025-04-29 21:25:00 +0200

    fix: Translation

--- src/i18n.js
+++ src/i18n.js
@@ -8,7 +8,7 @@ import { createI18n } from "vue-i18n";
  */
 function loadLocaleMessages() {
   const locales = require.context(
-    "./locales",
+    "@/locales",
     true,
     /[A-Za-z0-9-_,\s]+\.json$/i
   );
@@ -17,7 +17,7 @@ function loadLocaleMessages() {
     const matched = key.match(/([A-Za-z0-9-_]+)\./i);
     if (matched && matched.length > 1) {
       const locale = matched[1];
-      messages[locale] = locales(key).default;
+      messages[locale] = locales(key);
     }
   });
   return messages;
