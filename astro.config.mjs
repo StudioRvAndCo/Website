4e9b422555b316daab7c0ffa9ffed1497a9896a2
Minarox <contact@minarox.fr>
2025-09-09 10:14:43 +0200

    feat: Add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
-
+import sitemap from '@astrojs/sitemap';
 import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
+  integrations: [sitemap()],
   adapter: cloudflare({
     platformProxy: {
       enabled: true
