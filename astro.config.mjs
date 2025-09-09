5dcf56254b87ac3680ece846987954bb44bb4e8a
Minarox <contact@minarox.fr>
2025-09-09 10:16:24 +0200

    feat: Add icons

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 import metaTags from 'astro-meta-tags';
+import icon from 'astro-icon';
 import compressor from 'astro-compressor';
 import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [sitemap(), metaTags(), compressor()],
+  integrations: [sitemap(), metaTags(), icon(), compressor()],
   adapter: cloudflare({
     platformProxy: {
       enabled: true
