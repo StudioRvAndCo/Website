d66f4d754b09ccd0015fd1ad5a943a41ac17a8b0
Minarox <contact@minarox.fr>
2025-09-09 10:15:57 +0200

    feat: Add compressor

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 import metaTags from 'astro-meta-tags';
+import compressor from 'astro-compressor';
 import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [sitemap(), metaTags()],
+  integrations: [sitemap(), metaTags(), compressor()],
   adapter: cloudflare({
     platformProxy: {
       enabled: true
