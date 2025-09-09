ffdd2acffe5ee6b864270d8b0ace7086175e4c33
Minarox <contact@minarox.fr>
2025-09-09 10:15:22 +0200

    feat: Add metaTags

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
+import metaTags from 'astro-meta-tags';
 import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [sitemap()],
+  integrations: [sitemap(), metaTags()],
   adapter: cloudflare({
     platformProxy: {
       enabled: true
